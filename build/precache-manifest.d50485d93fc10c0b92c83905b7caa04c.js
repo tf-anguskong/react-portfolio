3ebc5eca36342e8030472c97fb9c3f086a918ae4
Tyler Fleming <tyler@fleming.systems>
2020-09-07 16:46:41 -0700

    added build folder

---
+++ build/precache-manifest.d50485d93fc10c0b92c83905b7caa04c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da4c4f988106645168a5073bbc77bc6",
+    "url": "/tf-anguskong/react-portfolioindex.html"
+  },
+  {
+    "revision": "35c6475934e28f365ba3",
+    "url": "/tf-anguskong/react-portfoliostatic/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/tf-anguskong/react-portfoliostatic/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/tf-anguskong/react-portfoliostatic/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c6475934e28f365ba3",
+    "url": "/tf-anguskong/react-portfoliostatic/js/main.9afa35d9.chunk.js"
+  },
+  {
+    "revision": "73c8a7a9e8a19d600353",
+    "url": "/tf-anguskong/react-portfoliostatic/js/runtime-main.e075101a.js"
+  }
+]);
